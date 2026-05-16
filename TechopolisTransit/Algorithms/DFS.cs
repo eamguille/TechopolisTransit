692283b5532a0255d1d9b1159840155dc5eb0b5f
eamguille <eamguille@gmail.com>
2026-05-15 21:03:58 -0600

    Inicializando proyecto con estructura de ficheros

---
+++ TechopolisTransit/Algorithms/DFS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechopolisTransit.Algorithms
+{
+    internal class DFS
+    {
+    }
+}
---
+++ TechopolisTransit/Models/Estacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace TechopolisTransit.Models
+{
+    public class Estacion
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Estacion(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public override bool Equals(object? obj) =>
+            obj is Estacion other && Id == other.Id;
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
